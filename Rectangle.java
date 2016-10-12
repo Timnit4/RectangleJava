c74644f3ed8c5196fa2c2718ce1b1b1e28edab20
Timnit Tesfaye <timnittesfaye@Timnits-Air.home>
2016-10-11 20:40:48 -0400

    created rectangle java

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Rectangle{
+  public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter the length of Rectangle");
+	double length = scanner.nextDouble();
+	System.out.println("Enter the width of Rectangle:");
+	double width = scanner.nextDouble();
+	//Area = length*width;
+	double area = length*width;
+	System.out.println("Area of Rectangle is: " + area);
+	double perimeter = 2*length + 2*width;
+	System.out.println("Perimeter of Rectangle is: " + perimeter);
+   }
+}
+
